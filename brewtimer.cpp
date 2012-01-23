dbfdb42c7c362f0275da1f048a6885543a9e663e
Eric Nelson <eric@crunch>
2012-01-23 16:04:11 -0500

    working on xml parser

--- brewtimer.cpp
+++ brewtimer.cpp
@@ -6,8 +6,8 @@
 #include <QDomDocument>
 
 BrewTimer::BrewTimer(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::BrewTimer)
+        QMainWindow(parent),
+        ui(new Ui::BrewTimer)
 {
     ui->setupUi(this);
 }
@@ -17,6 +17,7 @@ BrewTimer::~BrewTimer()
     delete ui;
 }
 
+void printNode(QDomNode *node);
 void BrewTimer::on_actionOpen_triggered()
 {
     QString name =  QFileDialog::getOpenFileName(this, tr("Open BeerXML File"), "$HOME", tr("BeerXML Files (*.xml)"));
@@ -25,6 +26,27 @@ void BrewTimer::on_actionOpen_triggered()
         return;
     QDomDocument *recipe = new QDomDocument("beerxml");
     recipe->setContent(&xml_file);
-    xml_file.close();
+    printNode(recipe);
 
+
+}
+int ilvl = 0;
+void printNode(QDomNode *node){
+    QString indent("");
+    for(int i = 0; i < ilvl; i ++){
+        indent += "\t";
+    }
+    QString name = node->nodeName();
+    QString value = node->nodeValue();
+    if(node->childNodes().count() == 1){
+        value = node->childNodes().item(1).nodeValue();
+        qDebug() << indent << "<" << name << ">={" << value << "}";
+    } else {
+        qDebug() << indent << "<" << name << ">={" << value << "}";
+        ilvl++;
+        for(int i = 0;  i < node->childNodes().count(); i++){
+            printNode(&node->childNodes().item(i));
+        }
+        ilvl--;
+    }
 }
--- brewtimer.pro
+++ brewtimer.pro
@@ -1,23 +1,15 @@
-#-------------------------------------------------
-#
+# -------------------------------------------------
 # Project created by QtCreator 2012-01-23T14:57:12
-#
-#-------------------------------------------------
-
-QT       += core gui xml
-
+# -------------------------------------------------
+QT += core \
+    gui \
+    xml
 TARGET = brewtimer
 TEMPLATE = app
-
-
-SOURCES += main.cpp\
-        brewtimer.cpp
-
-HEADERS  += brewtimer.h
-
-FORMS    += brewtimer.ui
-
-INCLUDEPATH +=
-
-OTHER_FILES += \
-    README.md
+SOURCES += main.cpp \
+    brewtimer.cpp
+HEADERS += brewtimer.h
+FORMS += brewtimer.ui
+INCLUDEPATH += 
+OTHER_FILES += README.md \
+    recipes.xml
